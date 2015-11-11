28565ba09a261c6b52eb4ba40d36c708f4f5a5c9
Claudenir Machado <claudenir.machado@sensedia.com>
2015-11-10 23:29:46 -0200

    Add services

--- www/js/app.js
+++ www/js/app.js
@@ -6,88 +6,88 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'ngResource'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+        .run(function ($ionicPlatform) {
+            $ionicPlatform.ready(function () {
+                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+                // for form inputs)
+                if (window.cordova && window.cordova.plugins.Keyboard) {
+                    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                    cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
+                }
+                if (window.StatusBar) {
+                    // org.apache.cordova.statusbar required
+                    StatusBar.styleDefault();
+                }
+            });
+        })
 
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
+        .config(function ($stateProvider, $urlRouterProvider) {
+            $stateProvider
 
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
+                    .state('app', {
+                        url: '/app',
+                        abstract: true,
+                        templateUrl: 'templates/menu.html',
+                        controller: 'AppCtrl'
+                    })
 
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
+                    .state('app.search', {
+                        url: '/search',
+                        views: {
+                            'menuContent': {
+                                templateUrl: 'templates/search.html'
+                            }
+                        }
+                    })
 
-    .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.cotacoes', {
-      url: '/cotacoes',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/cotacoes.html',
-          controller: 'CotacoesCtrl'
-        }
-      }
-    })
+                    .state('app.browse', {
+                        url: '/browse',
+                        views: {
+                            'menuContent': {
+                                templateUrl: 'templates/browse.html'
+                            }
+                        }
+                    })
+                    .state('app.cotacoes', {
+                        url: '/cotacoes',
+                        views: {
+                            'menuContent': {
+                                templateUrl: 'templates/cotacoes.html',
+                                controller: 'CotacaoCtrl'
+                            }
+                        }
+                    })
 
-    .state('app.sinistros', {
-      url: '/sinistros',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/sinistros.html',
-          controller: 'SinitrosCtrl'
-        }
-      }
-    })
+                    .state('app.cotacao', {
+                        url: '/cotacoes/:cotacaoId',
+                        views: {
+                            'menuContent': {
+                                templateUrl: 'templates/cotacao.html',
+                                controller: 'CotacaoCtrl'
+                            }
+                        }
+                    })
 
-    .state('app.cotacao', {
-      url: '/cotacoes/:cotacaoId',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/cotacao.html',
-          controller: 'CotacaoCtrl'
-        }
-      }
-    })
+                    .state('app.sinistros', {
+                        url: '/sinistros',
+                        views: {
+                            'menuContent': {
+                                templateUrl: 'templates/sinistros.html',
+                                controller: 'SinistroCtrl'
+                            }
+                        }
+                    })
 
-    .state('app.sinistro', {
-      url: '/sinistros/:sinistroId',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/sinistro.html',
-          controller: 'SinitroCtrl'
-        }
-      }
-    });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/cotacoes');
-});
+                    .state('app.sinistro', {
+                        url: '/sinistros/:sinistroId',
+                        views: {
+                            'menuContent': {
+                                templateUrl: 'templates/sinistro.html',
+                                controller: 'SinistroCtrl'
+                            }
+                        }
+                    });
+            // if none of the above states are matched, use this as the fallback
+            $urlRouterProvider.otherwise('/app/cotacoes');
+        });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,301 +1,275 @@
 var URL = 'https://1-dot-apix-carinsurance-cherri-02.appspot.com/_ah/api/carinsuranceendpoint';
-var URL_COTACOES = URL+'/v1/cotacoes';
-var URL_SINISTROS = URL+'/v1/sinistros';
 
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout, $http) {
+        .service('cotacaoService', ['$http', function ($http) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+                var URL_COTACAO = URL + '/v1/cotacoes';
 
-  // Form data for the login modal
-  $scope.loginData = {};
-
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.loginModal = modal;
-  });
-
-  $ionicModal.fromTemplateUrl('templates/newcotacao.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.loginNewCotacao = modal;
-  });
-
-  $ionicModal.fromTemplateUrl('templates/newsinistro.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.loginNewSinistro = modal;
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.loginModal.hide();
-  };
-
-  $scope.closeNewCotacao = function() {
-    $scope.loginNewCotacao.hide();
-  };
-
-  $scope.closeNewSinistro = function() {
-    $scope.loginNewSinistro.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.loginModal.show();
-  };
-
-  $scope.newCotacao = function() {
-    $scope.loginNewCotacao.show();
-  };
-
-  $scope.newSinistro = function() {
-    $scope.loginNewSinistro.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-
-  $scope.saveSinistro = function() {
-      var req = {
-        method: 'POST',
-        url: URL_SINISTROS,
-        //headers: { 'Content-Type': ""},
-        data: $scope.sinistro
-      }
-      $http(req)
-           .then(
-              function(resp) {                
-                $scope.sinistro = resp.data; 
-                console.log($scope.sinistro);
-                $scope.sinistro  = [];
-              }, 
-              function(err) {
-                console.log('erro');
-              }
-            )
-      
-      $timeout(function() {
-        $scope.closeNewSinistro();
-      }, 1000);
-    };
-
-    $scope.saveCotacao = function() {
-        var req = {
-          method: 'POST',
-          url: URL_COTACOES,
-          //headers: { 'Content-Type': ""},
-          data: $scope.cotacao
-        }
-        $http(req)
-             .then(
-                function(resp) {                
-                  $scope.cotacao = resp.data; 
-                  console.log($scope.cotacao);
-                  $scope.cotacao  = [];
-                }, 
-                function(err) {
-                  console.log('erro');
+                function findAll() {
+                    return $http({method: "GET", url: URL_COTACAO + "/?_limit=10&_offset=0"});
                 }
-              )
-        $timeout(function() {
-          $scope.closeNewCotacao();
-        }, 1000);
-      };
-})
 
-.controller('CotacoesCtrl', function($scope, $http) {
-    $scope.get = function() {
-      var req = {
-          method: 'GET',
-          url: URL_COTACOES + '?_limit=10&_offset=0'
-          //headers: { 'Content-Type': ""},
-          //data: { test: '' }
-      }
-      $http(req)
-           .then(
-              function(resp) {  
-                $scope.cotacoes = [];          
-                if(resp.data.items){
-                  resp.data.items.forEach(function(item) {
-                    $scope.cotacoes.push(item);
-                  });
-                }else{
-                  $scope.cotacoes.push({ title: 'Nenhum resultado encontrado', id: 0 });
-                }            
-              }, 
-              function(err) {
-                $scope.cotacoes = [
-                  { title: 'Erro ao recuperar informações das cotações', id: 0 }
-                ];
-              }
-            )
-    }
-
-    $scope.get();
-
-    $scope.deleteCotacao = function(id) {
-      var req = {
-        method: 'DELETE',
-        url: URL_COTACOES + "/" + id
-        //headers: { 'Content-Type': ""}
-      }
-      $http(req)
-           .then(
-              function(resp) {
-                  console.log('success');
-                  $scope.get();
-              }, 
-              function(err) {
-                  console.log('erro');
-              }
-            )  
-      console.log($scope.cotacoes);
-    };
-})
-
-.controller('CotacaoCtrl', function($scope, $http, $stateParams) {      
-      $scope.get = function() {
-        var req = {
-          method: 'GET',
-          url: URL_COTACOES+"/"+$stateParams.cotacaoId
-          //headers: { 'Content-Type': ""},
-          //data: { test: '' }
-        }
-        $http(req)
-             .then(
-                function(resp) {
-                  $scope.cotacao = resp.data; 
-                }, 
-                function(err) {
-                  console.log('erro');
+                function findById(id) {
+                    return $http({method: "GET", url: URL_COTACAO + "/" + id});
                 }
-              )  
-      }
-      
-      $scope.get();      
 
-      $scope.updateCotacao = function() {
-        var req = {
-          method: 'PUT',
-          url: URL_COTACOES+"/"+$scope.cotacao.id,
-          //headers: { 'Content-Type': ""},
-          data: $scope.cotacao
-        }
-        console.log(req.url);
-        $http(req)
-             .then(
-                function(resp) {                
-                  $scope.cotacao = resp.data; 
-                  console.log($scope.cotacao);
-                }, 
-                function(err) {
-                  console.log('erro');
+                function create(obj) {
+                    return $http({method: "POST", url: URL_COTACAO, data: obj});
                 }
-              )
-      };
-})
 
-.controller('SinitrosCtrl', function($scope, $http) {    
-    $scope.get = function() {
-      var req = {
-          method: 'GET',
-          url: URL_SINISTROS + '?_limit=10&_offset=0'
-          //headers: { 'Content-Type': ""},
-          //data: { test: '' }
-      }
-      $http(req)
-        .then(
-          function(resp) {  
-            $scope.sinistros = []; 
-            if(resp.data.items){
-              resp.data.items.forEach(function(item) {
-                $scope.sinistros.push(item);
-              });
-            }else{
-              $scope.sinistros.push({ title: 'Nenhum resultado encontrado', id: 0 });
-            }            
-          }, 
-          function(err) {
-            $scope.sinistros = [
-              { title: 'Erro ao recuperar informações dos sinistros', id: 0 }
-            ];
-          }
-        )  
-    }     
-    
-    $scope.get();
+                function update(obj) {
+                    return $http({method: "PUT", url: URL_COTACAO + "/" + obj.id, data: obj});
+                }
 
-    $scope.deleteSinistro = function(id) {
-      var req = {
-        method: 'DELETE',
-        url: URL_SINISTROS+"/"+id
-        //headers: { 'Content-Type': ""}
-      }
-      $http(req)
-           .then(
-              function(resp) {
-                console.log('sucess');
-              }, 
-              function(err) {
-                console.log('erro');
-              }
-            )  
-      console.log($scope.sinistros);
-    };      
-})
+                function remove(id) {
+                    return $http({method: "DELETE", url: URL_COTACAO + "/" + id});
+                }
 
-.controller('SinitroCtrl', function($scope, $http, $stateParams) {
-    var req = {
-      method: 'GET',
-      url: URL_SINISTROS+"/"+$stateParams.sinistroId
-      //headers: { 'Content-Type': ""},
-      //data: { test: '' }
-    }
-    console.log(req.url);
-    $http(req)
-         .then(
-            function(resp) {                
-              $scope.sinistro = resp.data; 
-              console.log($scope.sinistro);
-            }, 
-            function(err) {
-              console.log('erro');
-            }
-          )    
+                this.getCotacao = function (id) {
+                    return findById(id);
+                };
 
-    $scope.updateSinistro = function() {
-      var req = {
-        method: 'PUT',
-        url: URL_SINISTROS+"/"+$scope.sinistro.id,
-        //headers: { 'Content-Type': ""},
-        data: $scope.sinistro
-      }
-      $http(req)
-           .then(
-              function(resp) {                
-                $scope.sinistro = resp.data; 
-                console.log($scope.sinistro);
-              }, 
-              function(err) {
-                console.log('erro');
-              }
-            )
-    };
-});
+                this.getCotacoes = function () {
+                    return findAll();
+                };
+
+                this.deleteCotacao = function (id) {
+                    return remove(id);
+                };
+
+                this.saveCotacao = function (obj) {
+                    if (obj.id)
+                        return update(obj);
+                    else
+                        return create(obj);
+                };
+
+            }])
+
+        .service('sinistroService', ['$http', function ($http) {
+
+                var URL_SINISTRO = URL + '/v1/sinistros';
+
+                function findAll() {
+                    return $http({method: "GET", url: URL_SINISTRO + "/?_limit=10&_offset=0"});
+                }
+
+                function findById(id) {
+                    return $http({method: "GET", url: URL_SINISTRO + "/" + id});
+                }
+
+                function create(obj) {
+                    return $http({method: "POST", url: URL_SINISTRO, data: obj});
+                }
+
+                function update(obj) {
+                    return $http({method: "PUT", url: URL_SINISTRO + "/" + obj.id, data: obj});
+                }
+
+                function remove(id) {
+                    return $http({method: "DELETE", url: URL_SINISTRO + "/" + id});
+                }
+
+                this.getSinistro = function (id) {
+                    return findById(id);
+                };
+
+                this.getSinistros = function () {
+                    return findAll();
+                };
+
+                this.deleteSinistro = function (id) {
+                    return remove(id);
+                };
+
+                this.saveSinistro = function (obj) {
+                    if (obj.id)
+                        return update(obj);
+                    else
+                        return create(obj);
+                };
+
+            }])
+
+        .controller('AppCtrl', function ($scope, $ionicModal, $timeout, $http) {
+
+            // With the new view caching in Ionic, Controllers are only called
+            // when they are recreated or on app start, instead of every page change.
+            // To listen for when this page is active (for example, to refresh data),
+            // listen for the $ionicView.enter event:
+            //$scope.$on('$ionicView.enter', function(e) {
+            //});
+
+            // Form data for the login modal
+            $scope.loginData = {};
+
+            // Create the login modal that we will use later
+            $ionicModal.fromTemplateUrl('templates/login.html', {
+                scope: $scope
+            }).then(function (modal) {
+                $scope.loginModal = modal;
+            });
+
+            // Triggered in the login modal to close it
+            $scope.closeLogin = function () {
+                $scope.loginModal.hide();
+            };
+
+            // Open the login modal
+            $scope.login = function () {
+                $scope.loginModal.show();
+            };
+
+            // Perform the login action when the user submits the login form
+            $scope.doLogin = function () {
+                console.log('Doing login', $scope.loginData);
+
+                // Simulate a login delay. Remove this and replace with your login
+                // code if using a login system
+                $timeout(function () {
+                    $scope.closeLogin();
+                }, 1000);
+            };
+
+        })
+
+        .controller('CotacaoCtrl', function ($scope, $ionicModal, cotacaoService, $stateParams) {
+
+            $ionicModal.fromTemplateUrl('templates/newcotacao.html', {scope: $scope}).then(
+                    function (modal) {
+                        $scope.loginNewCotacao = modal;
+                    });
+
+            $scope.newCotacao = function () {
+                $scope.cotacao = {};
+                $scope.loginNewCotacao.show();
+            };
+
+            $scope.closeNewCotacao = function () {
+                $scope.loginNewCotacao.hide();
+            };
+
+            $scope.getCotacoes = function () {
+                cotacaoService.getCotacoes().then(
+                        function (resp) {
+                            if (resp.data.items.length)
+                                $scope.cotacoes = resp.data.items;
+                            else
+                                $scope.cotacoes.push({title: 'Nenhum resultado encontrado', id: 0});
+                        },
+                        function (err) {
+                            $scope.cotacoes = [
+                                {title: 'Erro ao recuperar informações das cotações', id: 0}
+                            ];
+                        }
+                );
+            };
+
+            $scope.getCotacaoById = function () {
+                cotacaoService.getCotacao($stateParams.cotacaoId).then(
+                        function (resp) {
+                            $scope.cotacao = resp.data;
+                        },
+                        function (err) {
+                            console.log('erro');
+                        }
+                );
+            };
+
+            $scope.saveCotacao = function () {
+                cotacaoService.saveCotacao($scope.cotacao).then(
+                        function (resp) {
+                            $scope.cotacao = resp.data;
+                        },
+                        function (err) {
+                            console.log('erro');
+                        }
+                );
+//                 $timeout(function () {
+                $scope.closeNewCotacao();
+//                 }, 1000);
+            };
+
+            $scope.deleteCotacao = function (id) {
+                cotacaoService.deleteCotacao(id).then(
+                        function (resp) {
+                            $scope.getCotacoes();
+                        },
+                        function (err) {
+                            console.log('erro');
+                        }
+                );
+            };
+
+        })
+
+        .controller('SinistroCtrl', function ($scope, $ionicModal, sinistroService, $stateParams) {
+
+            $ionicModal.fromTemplateUrl('templates/newsinistro.html', {scope: $scope}).then(
+                    function (modal) {
+                        $scope.loginNewSinistro = modal;
+                    });
+
+            $scope.newSinistro = function () {
+                $scope.sinistro = {};
+                $scope.loginNewSinistro.show();
+            };
+
+            $scope.closeNewSinistro = function () {
+                $scope.loginNewSinistro.hide();
+            };
+
+            $scope.getSinistros = function () {
+                sinistroService.getSinistros().then(
+                        function (resp) {
+                            if (resp.data.items.length)
+                                $scope.sinistros = resp.data.items;
+                            else
+                                $scope.sinistros.push({title: 'Nenhum resultado encontrado', id: 0});
+                        },
+                        function (err) {
+                            $scope.sinistros = [
+                                {title: 'Erro ao recuperar informações dos sinistros', id: 0}
+                            ];
+                        }
+                );
+            };
+
+            $scope.getSinistro = function () {
+                sinistroService.getSinistro($stateParams.sinistroId).then(
+                        function (resp) {
+                            $scope.sinistro = resp.data;
+                        },
+                        function (err) {
+                            console.log('erro');
+                        }
+                );
+            };
+
+            $scope.saveSinistro = function () {
+                sinistroService.saveSinistro($scope.sinistro).then(
+                        function (resp) {
+                            $scope.sinistro = resp.data;
+                        },
+                        function (err) {
+                            console.log('erro');
+                        }
+                );
+
+//                $timeout(function () {
+                    $scope.closeNewSinistro();
+//                }, 1000);
+            };
+
+            $scope.deleteSinistro = function (id) {
+                sinistroService.deleteSinistro(id).then(
+                        function (resp) {
+                            $scope.getSinistros();
+                        },
+                        function (err) {
+                            console.log('erro');
+                        }
+                );
+            };
+        });
